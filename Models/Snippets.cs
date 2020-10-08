18f5a114caa2ee3603b5f4b188ec02c180980eb1
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-08 20:38:45 -0300

    1815-snippetClass

---
+++ Models/Snippets.cs
@@ -0,0 +1,10 @@
+namespace SnippetProject31.Models
+{
+    public class Snippet
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
