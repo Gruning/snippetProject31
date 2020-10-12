1b3b2fced63036130dd7962a1d2005675dab92d0
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-12 16:54:24 -0300

    create DB

---
+++ Migrations/20201012195138_creatingDataBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace snippetProject31.Migrations
+{
+    public partial class creatingDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Snippets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HowTo = table.Column<string>(maxLength: 200, nullable: false),
+                    Line = table.Column<string>(nullable: false),
+                    Platform = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Snippets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Snippets");
+        }
+    }
+}
--- Models/Snippet.cs
+++ Models/Snippet.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SnippetProject31.Models
 {
     public class Snippet
     {
-        public int Id { get; set; }
-        public string HowTo { get; set; }
-        public string Line { get; set; }
-        public string Platform { get; set; }
+        [Key] public int Id { get; set; }
+        [Required][MaxLength(200)] public string HowTo { get; set; }
+        [Required] public string Line { get; set; }
+        [Required] public string Platform { get; set; }
     }
 }
